aee68e43817360954bb23cf307c394d01ccf7358
kalpitjain <kalpit.jain1810@gmail.com>
2024-05-18 10:26:07 +0530

    first commit

---
+++ src/frontend/src/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./pages/App"
+import { AuthClient } from "@dfinity/auth-client";
+
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+        window.location.reload();
+      },
+    });
+  }
+};
+
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  const userPrincipal = identity._principal.toString();
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App loggedInPrincipal={userPrincipal}/>
+      </Router>
+    </React.StrictMode>
+  );
+}
+init();
